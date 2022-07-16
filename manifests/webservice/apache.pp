13922fa89bb895dbf4fae0dd400ab71add6fc9df
Evan Booyens <evanbooyens@gmail.com>
2022-07-16 13:25:49 +0200

    Update apache.pp

--- manifests/webservice/apache.pp
+++ manifests/webservice/apache.pp
@@ -58,7 +58,7 @@ class roundcube::webservice::apache (
     mpm_module          => $mpm_module,
   }
 
-  package { 'libapache2-mod-php':
+  package { 'libapache2-mod-php7.3':
     ensure => installed,
   }
 
